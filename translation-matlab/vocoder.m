ef3d75fa28797684fb413b775e3b8c13d30f499b
jurajh <haluskajuraj0@gmail.com>
2019-03-07 22:02:07 +0100

    simplify vocoder script

--- translation-matlab/vocoder.m
+++ translation-matlab/vocoder.m
@@ -13,9 +13,9 @@ y = input(:, 1)';
 winPeriod = 0.020;
 hopRate = 50;
 fftSize = 4096;
-maxPeaks = 20;
-fTol = 25;
-harmony = [0 4 7 11 14];
+maxPeaks = 50;
+fTol = 3;
+harmony = [0 7 13];
 
 winSize = floor(Fs * winPeriod);
 hopSize = floor(winSize * hopRate / 100);
@@ -38,13 +38,13 @@ end
 
 % 2. Processing
 O = zeros(size(Y, 1), size(Y, 2)); % output frames
-zu = ones(length(harmony), maxPeaks); % phase accumulator
-zuf = zeros(length(harmony), maxPeaks); % holds bin frequency
+prevPhases = ones(length(harmony), maxPeaks); % phase accumulator
+prevBinFreqs = zeros(length(harmony), maxPeaks); % holds bin frequency
 for fI = 1:size(Y, 2)
     % apply windowing function
     frame = Y(:, fI) .* hamming(winSize);
     
-    % take fourier transformis
+    % take fourier transform
     ft = fft(frame, fftSize);
     lHalf = ft(1:length(ft) / 2 + 1);
     
@@ -59,75 +59,54 @@ for fI = 1:size(Y, 2)
         pks = pks(1:maxPeaks);
         locs = locs(1:maxPeaks);
     end
-    
-    halves = zeros(length(lHalf), length(harmony));
         
+    newHalf = zeros(1, length(lHalf))';
+    
     for hI=1:length(harmony)
-        % keep will indicate which phases were matched
-        % newZu is 
+ 
         keep = [];
-        newZu = [];
-        newZuf = [];
+        keepBins = [];
 
         % loop each peak
-        newHalf = zeros(1, length(lHalf))';
         for pI=1:length(locs)
-            % estimate peaks frequency
-            fHz = (locs(pI) - 1) * Fs / fftSize;
-
             % calculate bin transposition factor for this peak
-            fWanted = fHz * 2^(harmony(hI)/12);
-            t = round((fWanted - fHz) / (Fs/fftSize));
+            newBin = round((locs(pI) - 1) * 2^(harmony(hI)/12));
+            t = newBin - (locs(pI) - 1);
 
             % try to find matching fWanted from previous frame
-            dw = 2*pi*t/fftSize; % delta
-            [m, l] = min(abs(zuf(hI, :) - fWanted));
-            z = 1;
+            [m, l] = min(abs(prevBinFreqs(hI, :) - newBin));
             if (m <= fTol && m >= -fTol)
-                % match - rotate phase
-                zu(hI, l) = zu(hI, l)*exp(1j*dw*hopSize);
-                z = zu(hI, l);
-                keep = [keep l];
+                % match with prev frequency
+                keep = [keep prevPhases(hI, l)];
+                keepBins = [keepBins prevBinFreqs(hI, l)];
             else
                 % not match
-                z = z*exp(1j*dw*hopSize);
-                newZu = [newZu z];
-                newZuf = [newZuf fWanted];
+                keep = [keep 1];
+                keepBins = [keepBins newBin];
             end
 
+            % phase rotation
+            dw = 2*pi*t/fftSize;
+            keep(end) = keep(end)*exp(1j*dw*hopSize);
+            
             % isolate region of influence
             [first, last] = findInfluenceRegion(locs(pI), mags, floor(length(lHalf) / 2));
 
             % translate peak
             if last + t <= length(newHalf) && first + t >= 1 
                 region = lHalf(first:last);
-                newHalf(first + t:last + t) = newHalf(first + t:last + t) + region * z;
+                newHalf(first + t:last + t) = newHalf(first + t:last + t) + 1/length(harmony) * region * keep(end);
             end
         end
 
         % replace not matched phase continuations with new ones
-        tempZu = [];
-        tempZuf = [];
-        for kI=1:length(keep)
-            tempZu = [tempZu zu(hI, keep(kI))];
-            tempZuf = [tempZuf zuf(hI, keep(kI))];
-        end
-        for nI=1:length(newZu)
-            tempZu = [tempZu newZu(nI)];
-            tempZuf = [tempZuf newZuf(nI)];       
-        end
-
-        zu(hI, :) = tempZu;
-        zuf(hI, :) = tempZuf;
-    
-        halves(:, hI) = newHalf;
-        
+        prevPhases(hI, :) = [keep ones(1, maxPeaks - length(keep))];
+        prevBinFreqs(hI, :) = [keepBins ones(1, maxPeaks - length(keepBins))];        
     end
     
     % ---------------------------------------------------------------------
     % reconstruct signal
-    mixed = 0.5*sum(halves, 2);
-    reflected = [mixed' conj(fliplr(mixed(2:end-1)'))]';
+    reflected = [newHalf' conj(fliplr(newHalf(2:end-1)'))]';
     reconstructed = real(ifft(reflected));
     O(:, fI) = reconstructed(1:winSize);
 end
